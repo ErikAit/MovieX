37649c155ec4ed0332e261de3a94773928c7c43f
Erik <eait84344@gmail.com>
2024-02-08 17:17:46 +0400

    added HomePage.jsx and called in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './layout/Layout'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Layout />}>
-
+          <Route path='/' element={<HomePage />} />
         </Route>
       </Routes>
     </div>
