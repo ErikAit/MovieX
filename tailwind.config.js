74fd274e359d860df8c1cfe20a8856046240872f
Erik <eait84344@gmail.com>
2024-02-08 15:49:36 +0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        main: '#FAFF00',
+        header: '#5E5E5E',
+        white: '#fff',
+        black: '#383838'
+      },
+    },
+  },
+  plugins: [],
+}
