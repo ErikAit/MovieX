c335b4e7cb2f2c289c7d22b9f68639c275ab162b
Erik <eait84344@gmail.com>
2024-02-08 17:10:36 +0400

    layout.jsx called in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './layout/Layout'
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Layout />}>
 
+        </Route>
+      </Routes>
     </div>
   )
 }
