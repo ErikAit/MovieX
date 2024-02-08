bd629ba612e2f00a097ae3275e89a7814328bf0d
Erik <eait84344@gmail.com>
2024-02-08 16:23:49 +0400

    added all header's base

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,8 @@ import Search from './search/Search'
 
 export default function Header() {
   return (
-    <header className='header w-[1920px] h-[1080px] bg-header'>
-      <div className='header-top flex items-center justify-between px-[80px] py-[100px]'>
+    <header className='header w-[1920px] h-[1080px] bg-header px-[130px]'>
+      <div className='header-top flex items-center justify-between py-[100px]'>
         <div>
           <h2 className='text-white text-[32px] font-[600]'>
             Movie
@@ -15,6 +15,26 @@ export default function Header() {
         <div>
           <Search />
         </div>
+      </div>
+
+      <div className='static-films-container flex justify-center gap-[20px]'>
+        <div className='relative'>
+          <div className='w-[820px] h-[500px] rounded-[30px] border-main border-[2px]'></div>
+          <button className='text-black bg-main font-[600] w-[160px] h-[60px] rounded-[50px] absolute left-[620px] top-[400px]'>Watch now</button>
+        </div>
+
+        <div>
+          <div className='w-[400px] h-[500px] rounded-[30px] border-main border-[2px]'></div>
+        </div>
+
+        <div>
+          <div className='w-[400px] h-[500px] rounded-[30px] border-main border-[2px]'></div>
+        </div>
+      </div>
+
+      <div className="content mt-[45px]">
+        <h2 className='text-main text-[30px] font-[600] mb-[45px]'>Watch everywhere.</h2>
+        <span className='text-[24px] text-white font-[500]'>Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.</span>
       </div>
     </header>
   )
--- src/components/Header/search/Search.jsx
+++ src/components/Header/search/Search.jsx
@@ -7,7 +7,7 @@ export default function Search() {
         type="text"
         name='search'
         id='search'
-        className='border-main border-[2px] bg-transparent w-[400px] h-[80px] rounded-[50px]'
+        className='border-main border-[2px] bg-transparent w-[400px] h-[80px] rounded-[50px] outline-none pl-2 text-white'
       />
       <div className='bx bx-search absolute right-8 text-[2rem] text-white'></div>
     </div>
