6858a64a43cbf64e98c7ee8b5dc70ac80f61c554
Erik <eait84344@gmail.com>
2024-02-08 16:10:27 +0400

    added search styles

---
+++ src/components/Header/search/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div className='search-box relative flex items-center'>
+      <input
+        type="text"
+        name='search'
+        id='search'
+        className='border-main border-[2px] bg-transparent w-[400px] h-[80px] rounded-[50px]'
+      />
+      <div className='bx bx-search absolute right-8 text-[2rem] text-white'></div>
+    </div>
+  )
+}
