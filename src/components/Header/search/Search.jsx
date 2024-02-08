0e4fd5163f7ef419c14e2332671d6f5a15af91ff
Erik <eait84344@gmail.com>
2024-02-08 20:17:05 +0400

    added state for the search input

--- src/components/Header/search/Search.jsx
+++ src/components/Header/search/Search.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
+
 export default function Search() {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <div className='search-box relative flex items-center'>
       <input
         type="text"
         name='search'
         id='search'
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
         className='border-main border-[2px] bg-transparent w-[400px] h-[80px] rounded-[50px] outline-none pl-2 text-white'
       />
       <div className='bx bx-search absolute right-8 text-[2rem] text-white'></div>
