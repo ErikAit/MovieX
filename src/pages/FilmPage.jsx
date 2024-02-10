a7b7920af8533e5e11edf287aef9a83320bd88fd
Erik <eait84344@gmail.com>
2024-02-10 15:29:08 +0400

    added loading for Film page

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Loading from '../components/loading/Loading';
 
 export default function FilmPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+
   return (
-    <div>FilmPage</div>
+    <div>
+      {
+        isLoading &&
+        <div className='fixed top-0 left-0 z-50 w-full'>
+          <Loading />
+          {
+            setTimeout(() => {
+              setIsLoading(false)
+            }, 600)
+          }
+        </div>
+      }
+    </div>
   )
 }
