4438a7af0c98e273d12e20be35bc9b2fa597c435
Erik <eait84344@gmail.com>
2024-02-11 13:54:16 +0400

    error when the similar films not found

--- src/components/similar-films/SimilarFilms.jsx
+++ src/components/similar-films/SimilarFilms.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Film from '../film-card/Film';
-import { Link } from 'react-router-dom';
-import Error from '../film-not-found/Error';
+import NotFound from '../film-not-found/Error'
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -14,9 +13,10 @@ function shuffleArray(array) {
 export default function SimilarFilms() {
   const [similar, setSimilar] = useState([]);
 
+  const empty = []
+
   const film_title = location.href.split('?')[2]
   const filmId = location.href.split('?')[1].replace('/', '')
-  console.log(film_title);
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/search/movie?query=${film_title}&api_key=d91b4b2e8fb2707acd809975c49bcf87&page=`)
@@ -30,24 +30,25 @@ export default function SimilarFilms() {
     <div className='my-[60px]'>
       <h2 className='font-[700] text-[40px] text-black text-center'>Similar films</h2>
       <div>
-        {
-          similar.length !== 0 ? (
-            <div className='film-card-contaier grid grid-cols-4 place-items-center mt-[30px] px-[8rem]'>
-              {
-                similar.map((film, index) => {
-                  if (index < 5 && film.id !== +filmId) {
-                    return (
-                      <div key={film.id} className='my-[20px]'>
-                        <Film film={film} />
-                      </div>
-                    )
-                  }
-                })
+        <div className='film-card-contaier h-[50vh] grid grid-cols-4 place-items-center mt-[30px] px-[8rem]'>
+          {
+            similar.map((film, index) => {
+              if (index < 5 && film.id !== +filmId && film.length !== 0) {
+                return (
+                  <div key={film.id} className='my-[20px]'>
+                    <Film film={film} />
+                  </div>
+                )
+              } else {
+                return (
+                  <div className='absolute'>
+                    <NotFound />
+                  </div>
+                )
               }
-            </div>
-          ) : <Error
-          />
-        }
+            })
+          }
+        </div>
       </div>
     </div >
   )
