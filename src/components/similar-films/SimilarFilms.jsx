8816659859dfe3d2b15a857430b67a0b5482d2f6
Erik <eait84344@gmail.com>
2024-02-14 19:30:42 +0400

    similar films are fixed

--- src/components/similar-films/SimilarFilms.jsx
+++ src/components/similar-films/SimilarFilms.jsx
@@ -1,23 +1,31 @@
 import React, { memo, useEffect, useState } from 'react';
 import Film from '../film-card/Film';
 import NotFound from '../film-not-found/Error';
+import Loading from '../loading/Loading';
 
 function SimilarFilms() {
   const [similar, setSimilar] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const film_title = location.href.split('?')[2];
   const filmId = location.href.split('?')[1].replace('/', '');
 
   useEffect(() => {
+    setIsLoading(true)
+
     fetch(`https://api.themoviedb.org/3/search/movie?query=${film_title}&api_key=d91b4b2e8fb2707acd809975c49bcf87&page=`)
       .then((res) => res.json())
       .then((res) => {
         const filteredSimilar = res.results.filter((film) => film.id !== parseInt(filmId));
         setSimilar(filteredSimilar.slice(0, 4));
-      });
-  }, [filmId, similar]);
+      }).finally(() => {
+        setIsLoading(false)
+      })
+  }, [filmId, film_title]);
 
   return (
     <div className='my-[60px]'>
+      {isLoading && <Loading />}
       <h2 className='font-[700] text-[40px] text-black text-center'>Similar films</h2>
       <div>
         {
--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,24 +1,8 @@
-import React, { useState } from 'react'
-import Loading from '../components/loading/Loading';
 import SimilarFilms from '../components/similar-films/SimilarFilms';
 
 export default function FilmPage() {
-  const [isLoading, setIsLoading] = useState(true);
-
-
   return (
     <div>
-      {
-        isLoading &&
-        <div className='fixed top-0 left-0 z-50 w-full'>
-          <Loading />
-          {
-            setTimeout(() => {
-              setIsLoading(false)
-            }, 600)
-          }
-        </div>
-      }
       <SimilarFilms />
     </div>
   )
