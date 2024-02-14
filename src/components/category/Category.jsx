276a8707a017d2892bbc86eb302da44280300094
Erik <eait84344@gmail.com>
2024-02-14 21:56:37 +0400

    added category

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -2,8 +2,18 @@ import React, { memo } from 'react'
 
 function Category() {
   return (
-    <div>
-
+    <div className='film-card-contaier grid gap-[18px] cd2:grid-cols-2 cd:grid-cols-4 place-items-center mt-[30px] px-[8rem]'>
+      <i
+        className='bx bxs-left-arrow text-[40px] text-main absolute left-[10px]'
+        onClick={() => handlePopularSlide('prev')}
+      ></i>
+      {popularMovies.slice(currentPopularIndex, currentPopularIndex + 4).map((popular) => (
+        <Film key={popular.id} film={popular} />
+      ))}
+      <i
+        className='bx bxs-right-arrow text-[40px] text-main absolute right-[10px]'
+        onClick={() => handlePopularSlide('next')}
+      ></i>
     </div>
   )
 }
