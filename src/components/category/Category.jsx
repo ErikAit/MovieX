27328d3163dfe223b7a3592b4910a701ff987fca
Erik <eait84344@gmail.com>
2024-02-14 22:04:12 +0400

    added Category in Home page

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,18 +1,28 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+import Film from '../film-card/Film'
 
-function Category() {
+function Category({ film }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleFilmSlide = (direction) => {
+    if (direction === 'prev') {
+      setCurrentIndex((prevIndex) => (prevIndex === 0 ? film.length - 1 : prevIndex - 1));
+    } else {
+      setCurrentIndex((prevIndex) => (prevIndex === film.length - 1 ? 0 : prevIndex + 1));
+    }
+  };
   return (
     <div className='film-card-contaier grid gap-[18px] cd2:grid-cols-2 cd:grid-cols-4 place-items-center mt-[30px] px-[8rem]'>
       <i
         className='bx bxs-left-arrow text-[40px] text-main absolute left-[10px]'
-        onClick={() => handlePopularSlide('prev')}
+        onClick={() => handleFilmSlide('prev')}
       ></i>
-      {popularMovies.slice(currentPopularIndex, currentPopularIndex + 4).map((popular) => (
-        <Film key={popular.id} film={popular} />
+      {film.slice(currentIndex, currentIndex + 4).map((film) => (
+        <Film key={film.id} film={film} />
       ))}
       <i
         className='bx bxs-right-arrow text-[40px] text-main absolute right-[10px]'
-        onClick={() => handlePopularSlide('next')}
+        onClick={() => handleFilmSlide('next')}
       ></i>
     </div>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Film from '../components/film-card/Film';
 import Loading from '../components/loading/Loading';
+import Category from '../components/category/Category';
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -14,8 +15,6 @@ export default function HomePage() {
   const [popularMovies, setPopularMovies] = useState([]);
   const [mostRatedMovies, setMostRatedMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [currentPopularIndex, setCurrentPopularIndex] = useState(0);
-  const [currentRatedIndex, setCurrentRatedIndex] = useState(0);
 
   useEffect(() => {
     setIsLoading(true)
@@ -36,22 +35,6 @@ export default function HomePage() {
       .finally(() => setIsLoading(false))
   }, []);
 
-  const handlePopularSlide = (direction) => {
-    if (direction === 'prev') {
-      setCurrentPopularIndex((prevIndex) => (prevIndex === 0 ? popularMovies.length - 1 : prevIndex - 1));
-    } else {
-      setCurrentPopularIndex((prevIndex) => (prevIndex === popularMovies.length - 1 ? 0 : prevIndex + 1));
-    }
-  };
-
-  const handleRatedSlide = (direction) => {
-    if (direction === 'prev') {
-      setCurrentRatedIndex((prevIndex) => (prevIndex === 0 ? mostRatedMovies.length - 1 : prevIndex - 1));
-    } else {
-      setCurrentRatedIndex((prevIndex) => (prevIndex === mostRatedMovies.length - 1 ? 0 : prevIndex + 1));
-    }
-  };
-
   return (
     <div className='Home my-[60px]'>
       {isLoading && (
@@ -59,34 +42,10 @@ export default function HomePage() {
       )}
 
       <h2 className='font-[700] text-[40px] text-black text-center'>Most popular films</h2>
-      <div className='film-card-contaier grid gap-[18px] cd2:grid-cols-2 cd:grid-cols-4 place-items-center mt-[30px] px-[8rem]'>
-        <i
-          className='bx bxs-left-arrow text-[40px] text-main absolute left-[10px]'
-          onClick={() => handlePopularSlide('prev')}
-        ></i>
-        {popularMovies.slice(currentPopularIndex, currentPopularIndex + 4).map((popular) => (
-          <Film key={popular.id} film={popular} />
-        ))}
-        <i
-          className='bx bxs-right-arrow text-[40px] text-main absolute right-[10px]'
-          onClick={() => handlePopularSlide('next')}
-        ></i>
-      </div>
+      <Category film={popularMovies} />
 
       <h2 className='font-[700] text-[40px] mt-[60px] text-black text-center'>Most rated films</h2>
-      <div className='film-card-contaier grid gap-[18px] cd2:grid-cols-2 cd:grid-cols-4 place-items-center mt-[30px] px-[8rem]'>
-        <i
-          className='bx bxs-left-arrow text-[40px] text-main absolute left-[10px]'
-          onClick={() => handleRatedSlide('prev')}
-        ></i>
-        {mostRatedMovies.slice(currentRatedIndex, currentRatedIndex + 4).map((rated) => (
-          <Film key={rated.id} film={rated} />
-        ))}
-        <i
-          className='bx bxs-right-arrow text-[40px] text-main absolute right-[10px]'
-          onClick={() => handleRatedSlide('next')}
-        ></i>
-      </div>
+      <Category film={mostRatedMovies} />
     </div>
   );
 }
