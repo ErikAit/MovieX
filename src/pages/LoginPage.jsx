5e6dc81806b96c454747084bb60506c006923a43
Erik <eait84344@gmail.com>
2024-02-16 19:05:06 +0400

    added error when the email or password are wrong

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,48 +5,56 @@ export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
   const emailRegexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
 
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault()
     if (email !== '' && password !== '' && emailRegexp.test(email)) {
       localStorage.setItem('isLoggedIn', 'true');
+    } else {
+      setEmailError(true);
+      setPasswordError(true);
     }
   };
 
   return (
     <>
       {
-        !localStorage.getItem('isLoggedIn') ? <div className='bg-header h-screen flex justify-center items-center'>
-          <form className='w-[420px] h-[560px] border-[3px] border-main rounded flex flex-col justify-center items-center'>
-            <div className='m-[15px]'>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder='Enter your email...'
-                className='px-[13px] py-[12px] bg-transparent text-white border-[2px] border-main rounded-[25px] outline-none'
-              />
-              {email === '' ? <div className='text-red-600'>Неверный адресс.</div> : null}
-            </div>
+        !localStorage.getItem('isLoggedIn') ?
+          <div className='bg-header h-screen flex justify-center items-center'>
+            <form className='w-[420px] h-[560px] border-[3px] border-main rounded flex flex-col justify-center items-center'>
+              <div className='m-[15px]'>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder='Enter your email...'
+                  className='px-[13px] py-[12px] bg-transparent text-white border-[2px] border-main rounded-[25px] outline-none'
+                />
+                {emailError ? <div className='text-red-600'>Неверный адресс.</div> : null}
+              </div>
 
-            <div className='m-[10px]'>
-              <input
-                type="text"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder='Enter your password...'
-                className='px-[13px] py-[12px] bg-transparent text-white border-[2px] border-main rounded-[25px] outline-none'
-              />
-              {password === '' ? <div className='text-red-600'>Неверный пароль.</div> : null}
-            </div>
+              <div className='m-[10px]'>
+                <input
+                  type="text"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder='Enter your password...'
+                  className='px-[13px] py-[12px] bg-transparent text-white border-[2px] border-main rounded-[25px] outline-none'
+                />
+                {passwordError ? <div className='text-red-600'>Неверный пароль.</div> : null}
+              </div>
 
-            <div className='m-[20px] border-[2px] border-main py-[8px] px-[18px] rounded-[20px] text-white'>
-              <Link to={'/'}>
-                <button onClick={handleLogin} type="submit">Submit</button>
-              </Link>
-            </div>
-          </form>
-        </div> : location.href = '/'
+              <div className='m-[20px] border-[2px] border-main py-[8px] px-[18px] rounded-[20px] text-white'>
+                <Link to={'/'}>
+                  <button onClick={handleLogin} type="submit">Submit</button>
+                </Link>
+              </div>
+            </form>
+          </div> : location.href = '/'
       }
     </>
   )
