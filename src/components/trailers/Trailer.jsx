df5936d69e7b284e0c9ae608c242f10326823f3a
Erik <eait84344@gmail.com>
2024-02-16 21:21:48 +0400

    trailers are fixed

--- src/components/trailers/Trailer.jsx
+++ src/components/trailers/Trailer.jsx
@@ -25,24 +25,28 @@ function Trailer() {
     <div className='py-[30px]'>
       <h2 className='text-center text-white text-[40px] font-[700]'>Trailers</h2>
       <div className='flex justify-between pt-[40px]'>
-        {trailers.slice(0, 4).map((video, index) => (
-          <div key={video.key} onClick={() => openVideoPopup(video.key)}>
-            <div className='relative'>
-              <img
-                src={`https://img.youtube.com/vi/${video.key}/0.jpg`}
-                alt="trailer"
-                className='cursor-pointer w-[400px] h-[280px] rounded-[30px] border-[3px] border-main'
-              />
-              <div className='overlay w-[397px] h-[277px] rounded-[30px] absolute top-0 bg-[#000] opacity-[0.6] flex justify-center items-center text-[80px]'>
-                <i className='bx bx-play-circle text-white'></i>
+        {trailers.map((video, index) => {
+          if (index < 4) {
+            return (
+              <div key={video.key} onClick={() => openVideoPopup(video.key)}>
+                <div className='relative'>
+                  <img
+                    src={`https://img.youtube.com/vi/${video.key}/0.jpg`}
+                    alt="trailer"
+                    className='cursor-pointer w-[400px] h-[280px] rounded-[30px] border-[3px] border-main'
+                  />
+                  <div className='overlay w-[397px] h-[277px] rounded-[30px] absolute top-0 bg-[#000] opacity-[0.6] flex justify-center items-center text-[80px]'>
+                    <i className='bx bx-play-circle text-white'></i>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            )
+          }
+        })}
       </div>
 
       {selectedVideo && (
-        <div className="popup w-full h-screen fixed top-0 left-0 bg-header">
+        <div className="popup z-[100] w-full h-screen fixed top-0 left-0 bg-header">
           <i onClick={closeVideoPopup} className='bx bx-x text-[40px] m-2 float-end text-white'></i>
           <div className='flex h-full justify-center items-center'>
             <iframe width="860" height="515" src={`https://www.youtube.com/embed/${selectedVideo}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
