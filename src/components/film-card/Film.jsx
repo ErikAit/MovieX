9816dc3911cddc1c58c57edb98cff450cda09770
Erik <eait84344@gmail.com>
2024-02-17 17:28:20 +0400

    fixed responsive in Film.jsx

--- src/components/film-card/Film.jsx
+++ src/components/film-card/Film.jsx
@@ -14,7 +14,7 @@ function Film({ film }) {
         <div className='ml-[25px] my-[30px] font-[700] text-[20px]'>{film.original_title}</div>
       </div>
       <div className='absolute bottom-[6px]'>
-        <div className='flex items-center w-[400px] justify-between font-[700]'>
+        <div className='flex items-center ct:w-[250px] sr:w-[400px] justify-between font-[700]'>
           <div className='ml-[25px]'>
             <div className='bx bx-time-five mr-1 text-[20px] text-main'></div>
             {film.release_date.toString().slice(0, 4)}
